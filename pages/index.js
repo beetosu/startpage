b807ab1710f66aaaacb6eda0d9ea2bc4fa41d41a
beetosu <cpwalsh@buffalo.edu>
2020-12-29 00:33:29 -0500

    basic framework for API call

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import styles from '../styles.module.css'
 import Title from '../components/title'
 import Panels from '../components/panels'
 import Head from 'next/head'
+import secrets from '../secrets'
 
-export default function Home() {
+export default function Home({cards}) {
+  console.log(cards)
   return (
     <div className={styles.app}>
       <Head>
@@ -16,4 +18,15 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://api.trello.com/1/boards/5e2dd4a6c080b050676a4a84/cards?key='+secrets.trello.key+"&token="+secrets.trello.token)
+  const cards = await res.json()
+
+  return {
+    props: {
+      cards,
+    },
+  }
 }
